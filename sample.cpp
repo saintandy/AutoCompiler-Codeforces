097623471d843c6f9e212fb89bc603461efab188
Vlad Tarniceru <vladtarniceru@yahoo.com>
2013-06-09 16:47:55 +0300

    added sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,43 @@
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <cctype>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <map>
+using namespace std;
+
+typedef long double lod;
+typedef long long lol;
+
+/*
+			#ifdef ONLINE_JUDGE
+			#else
+			    ifstream fin("input");
+				#define cin fin
+			#endif
+*/
+
+
+
+
+int main() {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
