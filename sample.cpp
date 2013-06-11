e3ea363d0644f3626f19afdb693e71e326d5c736
Vlad Tarniceru <vladtarniceru@yahoo.com>
2013-06-11 18:57:17 +0300

    added new sample

--- sample.cpp
+++ sample.cpp
@@ -1,28 +1,37 @@
-
 #include <algorithm>
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <cstring>
-#include <string>
-#include <cstdio>
-#include <cctype>
 #include <vector>
+#include <string>
+#include <cctype>
+#include <cmath>
 #include <queue>
+#include <deque>
 #include <cmath>
 #include <map>
+#include <set>
 using namespace std;
+
+#define oo 2100000000
+#define PI 3.14159265358979323
+#define eps 0.0000001
 
 typedef long double lod;
 typedef long long lol;
 
-/*
-			#ifdef ONLINE_JUDGE
-			#else
-			    ifstream fin("input");
-				#define cin fin
+
+#define DBG
+template<typename _typeVar, typename _typeAft>
+	inline void deb(_typeVar var, _typeAft aft) {
+		#ifdef ONLINE_JUDGE
+		#else
+			#ifdef DBG
+				cout << var << aft;
 			#endif
-*/
+		#endif
+	}
+
 
 
 
