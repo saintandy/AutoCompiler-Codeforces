34c4b1fc4e4fa73c94abee2e6110fcb17d9d6511
vlad <vladtarniceru@gmail.com>
2014-03-23 17:48:54 +0200

    added sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,41 +1,48 @@
-// headers
-	#include <algorithm>
-	#include <iostream>
-	#include <sstream>
-	#include <cstring>
-	#include <vector>
-	#include <string>
-	#include <cctype>
-	#include <cmath>
-	#include <queue>
-	#include <deque>
-	#include <cmath>
-	#include <map>
-	#include <set>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+#ifdef HOME
+	#ifndef AUTOCOMPILER
+		#define cin fin
+		ifstream fin("input");
+	#endif
 	
-// namespaces
-	using namespace std;
-	
-	
-// defines
-	#define oo 2100000000
-	#define PI 3.14159265358979323
-	#define eps 0.0000001
-	
-	
-// debugging
-	template<typename T1>
-		inline void deb(const T1 &);
-	template<typename T1, typename T2>
-		inline void deb(const T1 &, const T2 &);
-	template<typename T1, typename T2, typename T3>
-		inline void deb(const T1 &, const T2 &, const T3 &);
-	template<typename T1, typename T2, typename T3, typename T4>
-		inline void deb(const T1 &, const T2 &, const T3 &, const T4 &);
-	template<typename T1, typename T2, typename T3, typename T4, typename T5>
-		inline void deb(const T1 &, const T2 &, const T3 &, const T4 &, const T5 &);
-#define DBG	
-//----------------------------------------------------------------------------------
+	inline void debug() {
+	}
+	template <typename _type, typename ... type>
+		inline void debug(_type arg, const type&... next) {
+			cout << arg;
+			debug(next...);
+		}
+#else
+	inline void debug() {
+	}
+	template <typename _type, typename ... type>
+		inline void debug(_type arg, const type&... next) {
+		}
+#endif
+
+
+#define mp make_pair
+#define fi first
+#define se second
+
+typedef long long bint;
+
+// ---------- TEMPLATE CODE ENDS HERE ----------
 
 
 
@@ -43,52 +50,27 @@ int main() {
 	
 }
 
-//----------------------------------------------------------------------------------
 
-	template<typename T1>
-		inline void deb(const T1 &V1) {
-			#ifdef ONLINE_JUDGE
-			#else
-				#ifdef DBG
-					cout << V1;
-				#endif
-			#endif
-		}
-	template<typename T1, typename T2>
-		inline void deb(const T1 &V1, const T2 &V2) {
-			#ifdef ONLINE_JUDGE
-			#else
-				#ifdef DBG
-					cout << V1 << V2;
-				#endif
-			#endif
-		}
-	template<typename T1, typename T2, typename T3>
-		inline void deb(const T1 &V1, const T2 &V2, const T3 &V3) {
-			#ifdef ONLINE_JUDGE
-			#else
-				#ifdef DBG
-					cout << V1 << V2 << V3;
-				#endif
-			#endif
-		}
-	template<typename T1, typename T2, typename T3, typename T4>
-		inline void deb(const T1 &V1, const T2 &V2, const T3 &V3, const T4 &V4) {
-			#ifdef ONLINE_JUDGE
-			#else
-				#ifdef DBG
-					cout << V1 << V2 << V3 << V4;
-				#endif
-			#endif
-		}
-	template<typename T1, typename T2, typename T3, typename T4, typename T5>
-		inline void deb(const T1 &V1, const T2 &V2, const T3 &V3, const T4 &V4, const T5 &V5) {
-			#ifdef ONLINE_JUDGE
-			#else
-				#ifdef DBG
-					cout << V1 << V2 << V3 << V4 << V5;
-				#endif
-			#endif
-		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
